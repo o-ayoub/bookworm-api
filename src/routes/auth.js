6c2377d661637587b5433c503dce6664c895c5f6
Ouarka <contact@ouarka.fr>
2018-07-23 17:55:16 +0100

    #06 done

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import User from '../models/user';
+import User from '../models/User';
 
 const router = express.Router();
 
@@ -13,7 +13,18 @@ router.post("/", (req, res) => {
                 res.status(400).json({errors: {global: "Invalid credentials "}})
             }
         });
-})
+});
+
+router.post('/confirmation', (req, res) => {
+    const token = req.body.token;
+    User.findOneAndUpdate(
+        {confirmationToken: token},
+        {confirmationToken: '', confirmed: true},
+        {new: true}
+    ).then(user =>
+        user ? res.json({user: user.toAuthJSON()}) : res.status(400).json({})
+    );
+});
 
 export default router;
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import User from '../models/user';
+import User from '../models/User';
 import parseErrors from '../utils/parseErrors';
+import {sendConfirmationEmail} from '../mailer';
 
 const router = express.Router();
 
@@ -8,10 +9,16 @@ router.post("/", (req, res) => {
     const {email, password} = req.body.user;
     const user = new User({email});
     user.setPassword(password);
+    user.setConfirmationToken();
     user
         .save()
-        .then(_user => res.status(200).json({user: _user.toAuthJSON()}))
+        .then(_user => {
+            sendConfirmationEmail(_user);
+            res.status(200).json({user: _user.toAuthJSON()});
+        })
         .catch(err => res.status(400).json({errors: parseErrors(err.errors)}));
-})
+});
+
+
 
 export default router;
